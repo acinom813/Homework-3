ced81c0422d6227881d2df59509aa39070a43520
Monica Hill <monica.r.hill@gmail.com>
2020-12-16 21:34:53 -0500

    update files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Homework-3
+
+In this assignment, Java Script was used to create a password generator that would allow users to select certain elements that would be used to generate a password. 
---
+++ script.js
@@ -0,0 +1,127 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+function getPassword() {
+var password = '';
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialChars = ['!', '@', '#','$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '=', '<', '>', '/', '.', '|', '~', '?'];
+
+    var length = parseInt(
+        prompt('Select a password length. Choose a number between 8 and 128.')
+    );
+    if (isNaN(length)=== true) {
+        alert('Password length must be provided as a number.');
+        return;
+    }
+
+if (length < 8) {
+    alert('Length requirement too short. Please select a number between 8 and 128');
+    return;
+  }
+if (length > 128) {
+    alert('Length requirement too high. Please select a number between 8 and 128.');
+    return;
+  }
+var hasNumbers = confirm(
+    'Click OK to include numbers'
+  );
+  var hasUppercase = confirm(
+    'Click OK to include uppercase letters'
+  );
+  var hasLowercase = confirm(
+    'Click OK to include lowercase letters.'
+  );
+  var hasSpecialChars = confirm(
+    'Click OK to include special characters.'
+  );
+
+
+
+      if 
+        ((hasNumbers === false) &&  
+        (hasUppercase === false) &&
+        (hasLowercase === false) &&
+        (hasSpecialChars === false) 
+        ){
+        alert('Must select at least one character type');
+        return;
+      }
+
+      
+      var passwordChars = {
+        length: length,
+        hasNumbers: hasNumbers,
+        hasUppercase: hasUppercase,
+        hasLowercase: hasLowercase,
+        hasSpecialChars: hasSpecialChars,
+      };
+
+      return passwordChars;
+    }
+
+function getRandom(arr) {
+    var randIndex = Math.floor(Math.random() * arr.length);
+    var randElement = arr[randIndex];
+
+    return randElement;
+  }
+
+  // Function to generate password with user input
+  function makePassword()
+  {
+    var password = getPassword();
+    var finalPassword = [];
+    var possiblePassword = [];
+    var guaranteedPassword = [];
+  
+
+    if (password.hasNumbers) {
+      possiblePassword = possiblePassword.concat(numbers);
+      guaranteedPassword.push(getRandom(numbers));
+    }
+
+    if (password.hasUppercase) {
+      possiblePassword = possiblePassword.concat(upercase);
+      guaranteedPassword.push(getRandom(uppercase));
+    }
+
+    if (password.hasLowercase) {
+      possiblePassword = possiblePassword.concat(lowercase);
+      guaranteedPassword.push(getRandom(lowercase));
+    }
+
+    if (password.hasSpecialChars) {
+        possiblePassword = possiblePassword.concat(specialChars);
+        guaranteedPassword.push(getRandom(specialChars));
+      }
+  
+  for (var i = 0; i < password.length; i++) {
+    var possiblePassword = getRandom(possiblePassword);
+    finalPassword.push(possiblePassword);
+  }
+
+  
+  for (var i = 0; i < guaranteedPassword.length; i++) {
+    finalPassword[i] = guaranteedPassword[i];
+  }
+  return finalPassword.join(', ');
+}
+var generatePasswordBtn = document.querySelector('#generate');
+
+function writePassword() {
+  var password = makePassword();
+  var passwordText = document.querySelector('#password');
+
+  passText.value = 'Your password is' + password + '!';
+}
+
+// Add event listener to make-soup button
+generatePasswordBtn.addEventListener('click', writePassword);
+
+
+
+
+
